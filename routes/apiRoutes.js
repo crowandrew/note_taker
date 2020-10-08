6954b132cd98cc79918203054ad16c364060971b
Andrew Crow <andrew@crow.me>
2020-10-07 23:31:20 -0700

    added delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@
 // =============================================================
 const express = require("express");
 const fs = require("fs");
+const { request } = require("http");
 
 // Sets up the Express App
 // =============================================================
@@ -16,17 +17,17 @@ const router = express.Router();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// JSON Read and Writ Functions
+// JSON Read and Write Functions
 // =============================================================
 function jsonReader(filePath, cb) {
     fs.readFile(filePath, (err, fileData) => {
-        if(err){
+        if (err) {
             return cb && cb(err)
         }
         try {
             const object = JSON.parse(fileData)
-            return cb && cb(null,object)
-        } catch(err){
+            return cb && cb(null, object)
+        } catch (err) {
             return cb & cb(err)
         }
     })
@@ -42,44 +43,56 @@ router.get("/api/notes", function (req, res) {
         if (err) {
             console.log(err)
             return
-        } 
+        }
         // console.log(notes);
-        return res.json(notes); 
-    })    
+        return res.json(notes);
+    })
 });
 
 
 // Create New Notes - takes in JSON input
 router.post("/api/notes", function (req, res) {
-    
-    jsonReader("./db/db.json", (err, notes) =>{
-        if(err){
-            console.log("Error reading file:",err);
+
+    jsonReader("./db/db.json", (err, notes) => {
+        if (err) {
+            console.log("Error reading file:", err);
             return res.json(false)
-        }   
+        }
         const newNote = req.body;
-        if(notes.length === 0) {
-            newNote.id = 0
+        if (notes.length === 0) {
+            newNote.id = 1
             notes.push(newNote)
         } else {
-            newNote.id = Math.max.apply(Math, notes.map(function(note) { return note.id; })) + 1
+            newNote.id = Math.max.apply(Math, notes.map(function (note) { return note.id; })) + 1
             notes.push(newNote)
-        }        
-        fs.writeFile("./db/db.json", JSON.stringify(notes) , (err) => {
-            if(err){
+        }
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+            if (err) {
                 console.log("Error writing file:", err);
             }
         })
         return res.json(true)
     }
-    
-)
+
+    )
 });
 
-// Deleting a specific note
-router.get("api/notes/:id", function (req, res) {
-    // deletes specific note based on id    
-    console.log(req.body);
+router.delete("/api/notes/:id", function (req, res) {
+    jsonReader("./db/db.json", (err, notes) => {
+        if (err) {
+            console.log("Error reading file:", err);
+            return res.json(false)
+        }
+        notes.splice(notes.findIndex((element) => element.id == req.params.id), 1);
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+            if (err) {
+                console.log("Error writing file:", err);
+            }
+        })
+        return res.json(false)
+    }
+
+    )
 });
 
 // Exports router
